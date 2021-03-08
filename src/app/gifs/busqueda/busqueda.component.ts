eaf3847f60ea0b4e26195caa4df40a2c78d06f89
CesarLuilly <cesarluilly.garcia@hotmail.com>
2021-03-08 07:00:59 -0600

    Video 79 - @ViewChild Obtener referencias a objetos del HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  //                //En donde nosotros podemos buscar por
+  //                //  elementos HTML, directivas, clases, IDs
+  //                //  y lo va a asignar al elemento txtBuscar de 
+  //                //  de la clase.
+  //                //Operador !: le estamos diciendo a TypeScript
+  //                //  qye el valor de ElementRef nunca va a ser nulo.
+  //                //Nota. Siempre que veamos que una clase es de tipo 
+  //                //  generico, es importante especificar el Tipo,
+  //                //  que en este caso el tipo HTMLInputElement.
+  @ViewChild('txtBuscar') txtBuscar !: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  //------------------------------------------------------------------------------------------   
+  public buscar (
+  //                //Busqueda.
+
+  ) : void{
+    console.log(this.txtBuscar);
+    const valor = this.txtBuscar.nativeElement.value;
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
